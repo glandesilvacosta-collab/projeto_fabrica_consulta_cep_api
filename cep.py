9ee613896c26b70461c01f3773e0e99737d5b10a
Emanuel Mello <glandesilvacosta@gmail.com>
2025-10-29 16:30:31 -0300

    Add files via upload

---
+++ cep.py
@@ -0,0 +1,24 @@
+# Crie uma api que cunsulte o cep e informe o endereço
+
+import requests
+
+# Indicamos a url para consulta da apiS
+cep = input('Digite o CEP (somente números)')
+url = f'https://viacep.com.br/ws/{cep}/json/'
+
+# Fazemos a requisição
+resposta = requests.get(url)  # aqui estamos fazendo a requisição
+
+if resposta.status_code == 200:
+    dados = resposta.json()
+    if 'erro' not in dados:
+        print(f'CEP: {dados["cep"]}')
+        print(f'Logradouro: {dados["logradouro"]}')
+        print(f'Bairro: {dados["bairro"]}')
+        print(f'Cidade: {dados["localidade"]}')
+        print(f'Estado: {dados["uf"]}')
+    else:
+        print('CEP não foi encontrado')
+else:
+    print(f'Erro na requisição: {resposta.status_code}')
+    print(resposta.content)
